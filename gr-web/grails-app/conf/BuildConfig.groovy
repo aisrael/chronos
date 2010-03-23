6916eecc557a297a33787db295db3cb9535dc050
Alistair A. Israel <aisrael@gmail.com>
2010-03-23 17:51:34 +0800

    Now this fixes logging

--- gr-web/grails-app/conf/BuildConfig.groovy
+++ gr-web/grails-app/conf/BuildConfig.groovy
@@ -4,7 +4,7 @@ grails.project.test.reports.dir	= "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits( "global" ) {
+    inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
@@ -12,7 +12,7 @@ grails.project.dependency.resolution = {
     repositories {        
         grailsPlugins()
         grailsHome()
-
+        
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
@@ -24,16 +24,16 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+        
         // runtime 'mysql:mysql-connector-java:5.1.5'
-	compile 'opensymphony:quartz:1.7.3'
+        compile 'opensymphony:quartz:1.7.3'
     }
-
+    
 }
 
 grails.war.resources = { stagingDir ->
     // remove Quartz JAR (and use provided Quartz JAR)
-	delete(file:"${stagingDir}/WEB-INF/lib/quartz-1.7.3.jar")
+    delete(file:"${stagingDir}/WEB-INF/lib/quartz-1.7.3.jar")
     // remove SLF4J to Log4J binding for JBoss (uses provided SLF4J to JBoss binding) 
     delete(file:"${stagingDir}/WEB-INF/lib/slf4j-log4j12-1.5.8.jar")
 }
